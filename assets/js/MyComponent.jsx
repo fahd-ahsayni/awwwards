4d2f94893e3dca2d748d70b6c39154d89f200838
fahd ahsyani <fahdahsayni@gmail.com>
2023-10-11 23:23:45 +0200

    working at imagery section

---
+++ assets/js/MyComponent.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const MyComponent = () => {
+  return (
+    <div>MyComponent</div>
+  )
+}
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
+function valueSette() {
+  gsap.set("#nav a", { y: "-100%", opacity: 0 });
+  gsap.set("#home .parent .child", { y: "100%" });
+  gsap.set("#home .row img", { opacity: 0 });
+  gsap.set("#tech", { opacity: 0 });
+}
+
 function revealToSpan() {
   const revealElements = document.querySelectorAll(".reveal");
 
@@ -20,14 +27,14 @@ function revealToSpan() {
 function runLoaderAnimation() {
   const tl = gsap.timeline();
 
-  tl.from(".child span", {
+  tl.from("#loader .child span", {
     x: 100,
     stagger: 0.2,
     duration: 1.4,
     ease: Power3.easeInOut,
   });
-  tl.to(".parent .child", {
-    y: "-100%",
+  tl.to("#loader .parent .child", {
+    y: "-110%",
     duration: 1,
     ease: Circ.easeInOut,
   })
@@ -48,8 +55,42 @@ function runLoaderAnimation() {
       duration: 1,
       delay: -0.5,
       ease: Circ.easeInOut,
+      onComplete: () => {
+        animateHomePage();
+      },
+    });
+}
+
+function animateHomePage() {
+  const tl = gsap.timeline();
+
+  tl.to("#nav a", {
+    y: 0,
+    opacity: 1,
+    stagger: 0.05,
+    ease: Expo.easeInOut,
+  })
+    .to("#home .parent .child", {
+      y: 0,
+      stagger: 0.1,
+      duration: 1,
+      ease: Expo.easeInOut,
+      onComplete: () => {
+        tl.to("#tech", {
+          opacity: 1,
+          duration: 1,
+          ease: Expo.easeInOut,
+        })
+      }
+    })
+    .to("#home .row img", {
+      opacity: 1,
+      duration: 0.5,
+      delay: -0.5,
+      ease: Expo.easeInOut,
     });
 }
 
 revealToSpan();
 runLoaderAnimation();
+valueSette();
